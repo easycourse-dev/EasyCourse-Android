4d095e2a76b4532503c5ebb057acfa9f18bbc244
Mark Wen <markwenguojie94@gmail.com>
2016-10-20 21:08:39 -0400

    Added new activity

---
+++ app/src/main/java/com/example/markwen/easycourse/SignupInitialSetup.java
@@ -0,0 +1,19 @@
+package com.example.markwen.easycourse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mark Wen on 10/20/2016.
+ */
+
+public class SignupInitialSetup extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_initial_setup);
+        // Hide toolbar for this specific activity
+        getSupportActionBar().hide();
+    }
+}

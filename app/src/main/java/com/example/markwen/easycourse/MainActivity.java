d64318fc638d283db17c524727ac196a751a54c2
Mark Wen <markwenguojie94@gmail.com>
2016-10-27 23:14:48 -0400

    Added ViewPagerAdapter

--- app/src/main/java/com/example/markwen/easycourse/MainActivity.java
+++ app/src/main/java/com/example/markwen/easycourse/MainActivity.java
@@ -2,19 +2,32 @@ package com.example.markwen.easycourse;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
+import com.example.markwen.easycourse.components.ViewPagerAdapter;
+import com.example.markwen.easycourse.fragments.Rooms;
+import com.example.markwen.easycourse.fragments.User;
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationItem;
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
 import com.luseen.luseenbottomnavigation.BottomNavigation.OnBottomNavigationItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private ViewPager viewPager;
+    private ViewPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +35,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
         // Tabs setup
         BottomNavigationItem bottomNavigationItem = new BottomNavigationItem
-                ("Chats", ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_chatboxes);
+                ("Rooms", ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_chatboxes);
         BottomNavigationItem bottomNavigationItem1 = new BottomNavigationItem
                 ("User", ContextCompat.getColor(this, R.color.colorTabDefault), R.drawable.ic_contact_outline);
         bottomNavigationView.addTab(bottomNavigationItem);
@@ -39,10 +54,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Viewpager setup
-        
+        pagerAdapter.addFragment(new Rooms(), "Rooms");
+        pagerAdapter.addFragment(new User(), "User");
+        viewPager.setAdapter(pagerAdapter);
 
         // Connect Viewpager with tab
         ContextCompat.getColor(getApplicationContext(), R.color.colorAccent);
-        bottomNavigationView.setUpWithViewPager(yourPager , colorResources , imageResources);
+        bottomNavigationView.setUpWithViewPager(viewPager , colorResources , imageResources);
     }
 }

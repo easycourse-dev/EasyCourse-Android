c5275cf71b57fa283bed949eff524d43a07411d0
nisargkolhe <nisargkolhe@gmail.com>
2016-10-29 13:43:51 -0400

    Initial setup for signup courses page

---
+++ app/src/main/java/com/example/markwen/easycourse/components/SignupChooseCoursesAdapter.java
@@ -0,0 +1,25 @@
+package com.example.markwen.easycourse.components;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by nisarg on 29/10/16.
+ */
+
+public class SignupChooseCoursesAdapter extends RecyclerView.Adapter{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/markwen/easycourse/fragments/SignupChooseCourses.java
+++ app/src/main/java/com/example/markwen/easycourse/fragments/SignupChooseCourses.java
@@ -26,6 +26,8 @@ public class SignupChooseCourses extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup_choose_courses, container, false);
+        View rootView = inflater.inflate(R.layout.signup_choose_courses, container, false);
+
+        return rootView;
     }
 }

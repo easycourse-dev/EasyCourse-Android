93f2e6661f92e3fb117f2384683761e84939b145
nisargkolhe <nisargkolhe@gmail.com>
2016-10-29 17:14:14 -0400

    Added onClickListener to cards

--- app/src/main/java/com/example/markwen/easycourse/components/SignupChooseCoursesAdapter.java
+++ app/src/main/java/com/example/markwen/easycourse/components/SignupChooseCoursesAdapter.java
@@ -2,12 +2,15 @@ package com.example.markwen.easycourse.components;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.markwen.easycourse.R;
+import com.hanks.library.AnimateCheckBox;
 
 import java.util.ArrayList;
 
@@ -23,16 +26,20 @@ public class SignupChooseCoursesAdapter extends RecyclerView.Adapter<SignupChoos
         this.coursesList = coursesList;
     }
 
-    public static class CourseViewHolder extends RecyclerView.ViewHolder {
+    public static class CourseViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout courseHolder;
         CardView courseCardView;
         TextView nameTextView;
         TextView titleTextView;
+        AnimateCheckBox courseCheckBox;
 
         CourseViewHolder(View itemView) {
             super(itemView);
+            courseHolder = (RelativeLayout)itemView.findViewById(R.id.course_holder_layout);
             courseCardView = (CardView)itemView.findViewById(R.id.course_card_view);
             nameTextView = (TextView)itemView.findViewById(R.id.name_text);
             titleTextView = (TextView)itemView.findViewById(R.id.title_text);
+            courseCheckBox = (AnimateCheckBox)itemView.findViewById(R.id.course_check_box);
         }
     }
 
@@ -46,9 +53,20 @@ public class SignupChooseCoursesAdapter extends RecyclerView.Adapter<SignupChoos
     }
 
     @Override
-    public void onBindViewHolder(CourseViewHolder courseViewHolder, int i) {
+    public void onBindViewHolder(final CourseViewHolder courseViewHolder, int i) {
         courseViewHolder.nameTextView.setText(coursesList.get(i).getName());
         courseViewHolder.titleTextView.setText(coursesList.get(i).getTitle());
+        final int index = i;
+        courseViewHolder.courseHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("com.example.easycourse", "Clicked "+ index);
+                if(courseViewHolder.courseCheckBox.isChecked())
+                    courseViewHolder.courseCheckBox.setChecked(false);
+                else
+                    courseViewHolder.courseCheckBox.setChecked(true);
+            }
+        });
     }
 
     @Override

a672681e6d1ba3bcf30bbaad703ef433bee57da4
nisargkolhe <nisargkolhe@gmail.com>
2016-10-29 12:18:43 -0400

    Fixed shared preferences not working

--- app/src/main/java/com/example/markwen/easycourse/SignupLogin.java
+++ app/src/main/java/com/example/markwen/easycourse/SignupLogin.java
@@ -3,9 +3,9 @@ package com.example.markwen.easycourse;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
@@ -20,7 +20,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 
 import com.example.markwen.easycourse.utils.APIFunctions;
 import com.facebook.CallbackManager;
@@ -211,7 +210,7 @@ public class SignupLogin extends AppCompatActivity {
                                     userToken = header.toString().substring(header.toString().indexOf(":") + 2);
                             }
 
-                            sharedPref = getPreferences(Context.MODE_PRIVATE);
+                            sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                             SharedPreferences.Editor editor = sharedPref.edit();
                             editor.putString("userToken", userToken);
                             editor.putString("currentUser", response.toString());
@@ -262,7 +261,7 @@ public class SignupLogin extends AppCompatActivity {
                         }
 
                         // Store user at SharedPreferences
-                        sharedPref = getPreferences(Context.MODE_PRIVATE);
+                        sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                         SharedPreferences.Editor editor = sharedPref.edit();
                         editor.putString("userToken", userToken);
                         editor.putString("currentUser", response.toString());

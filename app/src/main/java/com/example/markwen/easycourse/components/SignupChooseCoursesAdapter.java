478bdd54645c90f9b93baaf82e007efbe46dfeca
nisargkolhe <nisargkolhe@gmail.com>
2016-10-29 16:25:01 -0400

    Worked on RecyclerView for Choose courses page

--- app/src/main/java/com/example/markwen/easycourse/components/SignupChooseCoursesAdapter.java
+++ app/src/main/java/com/example/markwen/easycourse/components/SignupChooseCoursesAdapter.java
@@ -1,25 +1,63 @@
 package com.example.markwen.easycourse.components;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.markwen.easycourse.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by nisarg on 29/10/16.
  */
 
-public class SignupChooseCoursesAdapter extends RecyclerView.Adapter{
+public class SignupChooseCoursesAdapter extends RecyclerView.Adapter<SignupChooseCoursesAdapter.CourseViewHolder>{
+
+    private ArrayList<Course> coursesList = new ArrayList<>();
+
+    public SignupChooseCoursesAdapter(ArrayList<Course> coursesList){
+        this.coursesList = coursesList;
+    }
+
+    public static class CourseViewHolder extends RecyclerView.ViewHolder {
+        CardView courseCardView;
+        TextView nameTextView;
+        TextView titleTextView;
+
+        CourseViewHolder(View itemView) {
+            super(itemView);
+            courseCardView = (CardView)itemView.findViewById(R.id.course_card_view);
+            nameTextView = (TextView)itemView.findViewById(R.id.name_text);
+            titleTextView = (TextView)itemView.findViewById(R.id.title_text);
+        }
+    }
+
+
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+    public CourseViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.signup_choose_courses_item, viewGroup, false);
+        CourseViewHolder courseViewHolder = new CourseViewHolder(v);
+        return courseViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(CourseViewHolder courseViewHolder, int i) {
+        courseViewHolder.nameTextView.setText(coursesList.get(i).getName());
+        courseViewHolder.titleTextView.setText(coursesList.get(i).getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return coursesList.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 }
--- app/src/main/java/com/example/markwen/easycourse/fragments/SignupChooseCourses.java
+++ app/src/main/java/com/example/markwen/easycourse/fragments/SignupChooseCourses.java
@@ -4,17 +4,27 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.markwen.easycourse.R;
+import com.example.markwen.easycourse.components.Course;
+import com.example.markwen.easycourse.components.SignupChooseCoursesAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Mark Wen on 10/18/2016.
  */
 
 public class SignupChooseCourses extends Fragment {
+    private RecyclerView mRecyclerView;
+    private SignupChooseCoursesAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+
     public SignupChooseCourses() {}
 
     public static SignupChooseCourses newInstance() {
@@ -24,6 +34,11 @@ public class SignupChooseCourses extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ArrayList<Course> courses = new ArrayList<>();
+        courses.add(new Course("CS17700","Programming With Multimedia Objects"));
+        courses.add(new Course("CS18000","Problem Solving And Object-Oriented Programming"));
+
+        mAdapter = new SignupChooseCoursesAdapter(courses);
     }
 
     @Override
@@ -31,6 +46,13 @@ public class SignupChooseCourses extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.signup_choose_courses, container, false);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.choose_courses_recycler_view);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
         return rootView;
     }

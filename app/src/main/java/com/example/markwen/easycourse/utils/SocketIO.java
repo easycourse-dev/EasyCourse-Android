7b920a4664e8b4e1204c6112694d3dcd9fccfab9
nisargkolhe <nisargkolhe@gmail.com>
2016-11-12 16:22:05 -0500

    syncUser saves User in realm now

--- app/src/main/java/com/example/markwen/easycourse/utils/SocketIO.java
+++ app/src/main/java/com/example/markwen/easycourse/utils/SocketIO.java
@@ -4,12 +4,21 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import com.example.markwen.easycourse.models.main.User;
+
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.UUID;
 
+import io.realm.Realm;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -60,12 +69,51 @@ public class SocketIO {
                     e.printStackTrace();
                 }
                 JSONObject obj = (JSONObject) args[0];
-                Log.e("com.example.easycourse", "got syncUser");
 
                 Log.e("com.example.easycourse", "" + obj);
+                byte[] avatar = null;
+                String avatarUrlString = "";
+                try {
+                    avatarUrlString = obj.getString("avatarUrl");
+                    URL avatarUrl = new URL(avatarUrlString);
+                    HttpURLConnection conn = (HttpURLConnection) avatarUrl.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+                    conn.setUseCaches(false);
+
+                    InputStream is = conn.getInputStream();
+
+                    try {
+                        avatar = IOUtils.toByteArray(conn.getInputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } catch (MalformedURLException e) {
+                    Log.e("com.example.easycourse", e.toString());
+                } catch (JSONException e) {
+                    Log.e("com.example.easycourse", e.toString());
+                } catch (IOException e) {
+                    Log.e("com.example.easycourse", e.toString());
+                }
+                Realm.init(context);
+                Realm realm = Realm.getDefaultInstance();
+
+                User user = null;
+                try {
+                    user = new User(obj.getString("_id"), obj.getString("displayName"), avatar, avatarUrlString, obj.getString("email"), obj.getString("university"));
+                } catch (JSONException e) {
+                    Log.e("com.example.easycourse", e.toString());
+                }
+
+                Log.e("com.example.easycourse", "user in realm? "+User.isUserInRealm(user, realm));
+                User.updateUserToRealm(user, realm);
+                Log.e("com.example.easycourse", "user in realm? "+User.isUserInRealm(user, realm));
+
             }
         });
-        
+
     }
 
 

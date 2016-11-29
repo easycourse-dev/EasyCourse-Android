4913976287eafd359846d7bf073c80decb7654a4
nisargkolhe <nisargkolhe@gmail.com>
2016-11-28 23:12:31 -0500

    Image selection almost working

--- app/src/main/java/com/example/markwen/easycourse/activities/UserProfile.java
+++ app/src/main/java/com/example/markwen/easycourse/activities/UserProfile.java
@@ -1,9 +1,20 @@
 package com.example.markwen.easycourse.activities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -29,10 +40,12 @@ import io.realm.Realm;
 
 public class UserProfile extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
     TextView textViewUsername;
     EditText editTextUsername;
     ImageButton editUsernameButton;
     FloatingActionButton saveChangesButton;
+    FloatingActionButton editAvatarButton;
 
     boolean isInEditMode = false;
 
@@ -40,6 +53,10 @@ public class UserProfile extends AppCompatActivity {
 
     Realm realm;
     SocketIO socket;
+
+    private static final int GALLERY_INTENT_CALLED = 1;
+    private static final int GALLERY_KITKAT_INTENT_CALLED = 2;
+    private String selectedImagePath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,6 +75,7 @@ public class UserProfile extends AppCompatActivity {
         textViewUsername = (TextView) findViewById(R.id.textViewUsername);
         editTextUsername = (EditText) findViewById(R.id.editTextUsername);
         saveChangesButton = (FloatingActionButton) findViewById(R.id.saveChangesButton);
+        editAvatarButton = (FloatingActionButton) findViewById(R.id.editAvatarButton);
         saveChangesButton.hide();
 
         editUsernameButton = (ImageButton) findViewById(R.id.editUsernameButton);
@@ -100,6 +118,33 @@ public class UserProfile extends AppCompatActivity {
             }
         });
 
+        editAvatarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(view.getContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                        Activity activity = new Activity();
+                        Context context = view.getContext();
+                        while (context instanceof ContextWrapper) {
+                            if (context instanceof Activity) {
+                                activity = (Activity)context;
+                            }
+                            context = ((ContextWrapper)context).getBaseContext();
+                        }
+
+                        ActivityCompat.requestPermissions(activity,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                } else{
+                    openGallery();
+                }
+
+
+            }
+        });
 
 
     }
@@ -121,6 +166,71 @@ public class UserProfile extends AppCompatActivity {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(editTextUsername.getWindowToken(),
                     InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK) return;
+        if (null == data) return;
+        Uri originalUri = null;
+        if (requestCode == GALLERY_INTENT_CALLED) {
+            originalUri = data.getData();
+        } else if (requestCode == GALLERY_KITKAT_INTENT_CALLED) {
+            originalUri = data.getData();
+        }
+
+        String path = getImagePath(originalUri);
+        Log.e("com.example.easycourse", path);
+
+    }
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getImagePath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery();
+
+                }
+                return;
+            }
+        }
+    }
+
+    private void openGallery(){
+        if (Build.VERSION.SDK_INT <19){
+            Intent intent = new Intent();
+            intent.setType("image/jpeg");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"),GALLERY_INTENT_CALLED);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/jpeg");
+            startActivityForResult(intent, GALLERY_KITKAT_INTENT_CALLED);
         }
     }
 }

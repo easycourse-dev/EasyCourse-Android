58a23d795e9868b4cb80e665419a531e87cdbcd0
Noah Rinehart <rinehart.noah@gmail.com>
2017-01-26 00:45:45 -0500

    some work

--- app/src/main/java/io/easycourse/www/easycourse/fragments/main/ChatImageViewFragment.java
+++ app/src/main/java/io/easycourse/www/easycourse/fragments/main/ChatImageViewFragment.java
@@ -1,12 +1,21 @@
 package io.easycourse.www.easycourse.fragments.main;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -14,9 +23,14 @@ import android.widget.Toast;
 
 import io.easycourse.www.easycourse.R;
 import io.easycourse.www.easycourse.utils.BitmapUtils;
+
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 import butterknife.BindView;
@@ -34,6 +48,7 @@ public class ChatImageViewFragment extends Fragment {
     private String url;
     private byte[] imageData;
     PhotoViewAttacher photoAttacher;
+    Vibrator vibrator;
 
     @BindView(R.id.relative_layout_chat_image)
     RelativeLayout relativeLayout;
@@ -43,6 +58,12 @@ public class ChatImageViewFragment extends Fragment {
     ImageView chatImageView;
     @BindView(R.id.chatImageProgressBar)
     ProgressBar chatProgressBar;
+    @BindView(R.id.chatImageViewShare)
+    ImageButton chatImageViewShare;
+    @BindView(R.id.chatImageViewForward)
+    ImageButton chatImageViewForward;
+    @BindView(R.id.chatImageViewDownload)
+    ImageButton chatImageViewDownload;
 
     public static ChatImageViewFragment newInstance(String url, byte[] imageData) {
         ChatImageViewFragment fragment = new ChatImageViewFragment();
@@ -58,6 +79,9 @@ public class ChatImageViewFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_chat_image_view, container, false);
         ButterKnife.bind(this, v);
 
+        vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+
+        chatImageView.setDrawingCacheEnabled(true);
 
 
         Callback imageLoadedCallback = new Callback() {
@@ -91,10 +115,71 @@ public class ChatImageViewFragment extends Fragment {
 
         Picasso.with(getContext()).load(url).into(chatImageView, imageLoadedCallback);
 
+        chatImageViewShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chatImageViewShare.setEnabled(false);
+                vibrator.vibrate(20);
+                if (url != null) {
+                    shareImageFromUrl();
+                } else {
+                    shareImageFromData();
+                }
+                chatImageViewShare.setEnabled(true);
+
+
+            }
+        });
+
+
+        chatImageViewDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 
         return v;
 
 
+    }
+
+    private void shareImageFromUrl() {
+        Uri bmpUri = getLocalBitmapUri(chatImageView);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.setType("image/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        }
+    }
+
+//    http://stackoverflow.com/questions/23527767/open-failed-eacces-permission-denied
+    private void shareImageFromData() {
+//                Bitmap bitmap = chatImageView.getDrawingCache();
+//                File root = Environment.getExternalStorageDirectory();
+//                File cachePath = null;
+//                try {
+//                    cachePath = File.createTempFile("temp", "jpg", getContext().getCacheDir());
+//                    cachePath.createNewFile();
+//                    FileOutputStream ostream = new FileOutputStream(cachePath);
+//                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+//                    ostream.close();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//
+//                if (cachePath != null) {
+//                    Intent share = new Intent(Intent.ACTION_SEND);
+//                    share.setType("image/*");
+//                    share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(cachePath));
+//
+//                    startActivity(Intent.createChooser(share, "Share Image"));
+//                }
     }
 
     private PhotoViewAttacher getPhotoViewAttacher() {
@@ -111,4 +196,30 @@ public class ChatImageViewFragment extends Fragment {
     private void closeFragment() {
         getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
     }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
 }
